fec8c453f9648789f08d53e3dc946187664d049e
jeevan <coredreams2058@gmail.com>
2025-03-14 17:59:22 +0545

    admin sidebar structuring initialized

---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,12 @@
+<aside class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0">
+    <div class="w-[100%] bg-red-500">
+        <ul class="bg-red-500">
+            <li><a href="">hello</a></li>
+            <li><a href="">hello</a></li>
+            <li><a href="">hello</a></li>
+            <li><a href="">hello</a></li>
+            <li><a href="">hello</a></li>
+            <li><a href="">hello</a></li>
+        </ul>
+    </div>
+</aside>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('aside', function (){
+    return view('admin.layout.mainLayout');
+});

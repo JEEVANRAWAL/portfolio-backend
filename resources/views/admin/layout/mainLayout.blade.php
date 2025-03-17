a785040955aeb654078c7fe0b55fdb3eb7fa5857
jeevan <coredreams2058@gmail.com>
2025-03-17 17:46:37 +0545

    nav items styled

--- resources/views/admin/layout/mainLayout.blade.php
+++ resources/views/admin/layout/mainLayout.blade.php
@@ -6,9 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css"> --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css" />
+
+     <!-- Fonts -->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('css/admin.css')}}">
 </head>
 <body>
     @include('admin.layout.sidebar')
-
+    
+    <script src="{{ asset('js/adminjs.js')}}"></script>
 </body>
 </html>
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1,12 +1,24 @@
-<aside class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0">
-    <div class="w-[100%] bg-red-500">
-        <ul class="bg-red-500">
-            <li><a href="">hello</a></li>
-            <li><a href="">hello</a></li>
-            <li><a href="">hello</a></li>
-            <li><a href="">hello</a></li>
-            <li><a href="">hello</a></li>
-            <li><a href="">hello</a></li>
+<aside id="sidebar" class="fixed top-0 left-0 z-40 w-[63px] h-screen pt-5 border-r border-gray-200 -translate-x-full sm:translate-x-0 transition-all ease-in-out duration-700 hover:w-[256px] group">
+    <div id="sidebar-info-container" class="sidebar-info-container mb-10 bg-red-500 flex items-center justify-center">
+        <div class="imagebox">
+            <img src="{{ asset('images/logo.jpg')}}" alt="">
+        </div>
+        <div id="expand-collapse" class="expand-collapse">
+            <i class="ti ti-layout-sidebar-left-expand text-3xl"></i>
+        </div>
+    </div>
+    <div class="w-[90%] m-auto">
+        <ul class="vertical-menu ">
+            <li class="hover:bg-red-100 px-4 py-2 mb-1 rounded-[8px]"><a href=""><i class="ti ti-home-2 text-[22px]"></i> <span class="item"> Dashboard</span></a></li>
+            <li class="hover:bg-red-100 px-4 py-2 mb-1 rounded-[8px]"><a href=""><i class="ti ti-menu-deep text-[22px]"></i> <span class="item"> Menus</span></a></li>
+            <li class="hover:bg-red-100 px-4 py-2 mb-1 rounded-[8px] active"><a href=""><i class="ti ti-layout-dashboard text-[22px]"></i><span class="item"> Pages</span></a></li>
+            <li class="hover:bg-red-100 px-4 py-2 mb-1 rounded-[8px]"><a href=""><i class="ti ti-article text-[22px]"></i> <span class="item"> Blogs</span></a></li>
+            <li class="hover:bg-red-100 px-4 py-2 mb-1 rounded-[8px]"><a href=""><i class="ti ti-article text-[22px]"></i> <span class="item"> hello</span></a></li>
+            <li class="hover:bg-red-100 px-4 py-2 mb-1 rounded-[8px]"><a href=""><i class="ti ti-article text-[22px]"></i> <span class="item"> hello</span></a></li>
         </ul>
     </div>
+
+    {{-- <div class="absolute top-[50%] left-[80%]">
+        <i class="ti ti-braille text-3xl"></i>
+    </div> --}}
 </aside>
